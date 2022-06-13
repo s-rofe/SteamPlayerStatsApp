f23f6bde2285543d4f75f44327b1c3e7c8731d76
s-rofe <40592658+s-rofe@users.noreply.github.com>
2022-06-13 19:08:49 +1200

    Add project files.

---
+++ Controllers/GamerStatsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GamerStatsApp.Helpers;
+
+namespace GamerStatsApp.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class GamerStatsController : Controller
+    {
+
+        [HttpGet("hello")]
+        public ActionResult hello() => Ok("Hello");
+   
+        [HttpGet("GetVersion")]
+        public ActionResult GetVersion() => Ok("v 0.1.0");
+    }
+}
---
+++ Helpers/SteamHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace GamerStatsApp.Helpers
+{
+    public class SteamHelper
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<string> RetrieveSteamData()
+        {
+            try
+            {
+                var steamKey = "1A15650A96D9BF0C2009DFC25946361E";
+                var steam_id = "76561198093074081";
+                var url = String.Format("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={0}&steamid={1}&format=json", steamKey, steam_id);
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                return String.Format("\nException Caught! \nMessage :{0} ", e.Message);
+            }
+        }
+    }
+}
