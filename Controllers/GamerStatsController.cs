3e45219c48fc5c83e53c3e86ecb6661ba88c5966
s-rofe <40592658+s-rofe@users.noreply.github.com>
2022-07-11 18:25:55 +1200

    Implemented GetSteamData
    GetSteamData API returns all playtime and game information for a user in JSON format

--- Controllers/GamerStatsController.cs
+++ Controllers/GamerStatsController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using GamerStatsApp.Helpers;
+﻿using GamerStatsApp.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace GamerStatsApp.Controllers
 {
@@ -7,18 +8,51 @@ namespace GamerStatsApp.Controllers
     [ApiController]
     public class GamerStatsController : Controller
     {
+        private readonly KeyReader KeyReader;
+        private readonly SteamHelper SteamHelper;
 
-        [HttpGet("hello")]
-        public ActionResult hello() => Ok("Hello");
-   
+        public GamerStatsController()
+        {
+            KeyReader = new KeyReader();
+            SteamHelper = new SteamHelper();
+        }
+
+        // Get the current version of the backend
         [HttpGet("GetVersion")]
         public ActionResult GetVersion() => Ok("v 0.1.0");
 
+
+        // Get all Steam game and playtime data for a Steam user
+        // Returns a JSON reprosentation
         [HttpGet("GetSteamData/steam_id")]
 
-        public ActionResult GetSteamData()
+        public dynamic GetSteamData(string steamId)
         {
-            return Ok();
+            string steamKey = KeyReader.ReadSteamKey();
+            if (steamKey.Contains("Error"))
+            {
+                ContentResult r = new ContentResult
+                {
+                    Content = String.Format("Whoops there was a problem: {0}.", steamKey),
+                    ContentType = "text/plain;  charset=utf-8",
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                };
+                return r;
+            }
+
+            // Try and request the data from the Steam API
+            try
+            {
+
+                return Ok(SteamHelper.RetrieveSteamData(steamKey, steamId).Result);
+
+
+            }
+            catch (HttpRequestException e)
+            {
+                return e;
+            }
+
         }
     }
 }
