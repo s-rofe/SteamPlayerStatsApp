351aab3666c18bcacb5082d343d2f2c08deea46d
s-rofe <40592658+s-rofe@users.noreply.github.com>
2022-07-11 18:23:20 +1200

    Update SteamHelper.cs
    Added the deserialization of the responseBody for readability of the GetSteamData API response and for later use.

--- Helpers/SteamHelper.cs
+++ Helpers/SteamHelper.cs
@@ -1,25 +1,33 @@
-﻿namespace GamerStatsApp.Helpers
+﻿using GamerStatsApp.Models;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace GamerStatsApp.Helpers
 {
     public class SteamHelper
     {
-        private static readonly HttpClient client = new HttpClient();
+        private static HttpClient client = new HttpClient();
 
-        public static async Task<string> RetrieveSteamData(string steamKey)
+        public static async Task<dynamic> RetrieveSteamData(string steamKey, string steamId)
         {
             try
             {
-                var steam_id = "76561198093074081";
-                var url = String.Format("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={0}&steamid={1}&format=json", steamKey, steam_id);
+                var url = String.Format("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={0}&steamid={1}&format=json", steamKey, steamId);
                 HttpResponseMessage response = await client.GetAsync(url);
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
 
-                return responseBody;
+                dynamic data = JsonSerializer.Deserialize<dynamic>(responseBody);
+
+                return data;
+
             }
-            catch (HttpRequestException e)
+            catch (Exception e)
             {
                 return String.Format("\nException Caught! \nMessage :{0} ", e.Message);
             }
+
         }
     }
 }
